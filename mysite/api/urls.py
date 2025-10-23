697d74391f6563ec96195ceca897435448bd3069
megan <megansaml@btinternet.com>
2025-10-23 22:07:13 +0100

    custom view

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blogposts/", views.BlogPostListCreate.as_view(), name="blockpost-view-create")
+    path("blogposts/", views.BlogPostListCreate.as_view(), name="blockpost-view-create"),
+    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update"),
+    path("blogposts/search/", views.BlogPostList.as_view(), name="personal")
 ]
